1711fc0b35aac87a566d5a8b15c5359388a52a71
gtvardovsky <gtvardovsky@gmail.com>
2019-02-26 02:37:08 +0300

    Добавлены перемещения виджетов вверх-вниз и отображение имени файла

--- gui/src/VideoCutter.cpp
+++ gui/src/VideoCutter.cpp
@@ -8,12 +8,12 @@
 #include "ui_VideoCutter.h"
 #include "RangeSlider.h"
 
-VideoCutter::VideoCutter(QWidget *parent, int spinBoxValue, QString endTime, QString VideoFilePath) :
+VideoCutter::VideoCutter(QWidget *parent, int NumberInList, QString endTime, QString VideoFilePath) :
     QWidget(parent),
     ui(new Ui::VideoCutter) {
     ui->setupUi(this);
     videoFilePath = VideoFilePath;
-    ui->spinBox->setValue(spinBoxValue);
+    numberInList = NumberInList;
     ui->endTime->setText(endTime);
 }
 
@@ -21,29 +21,59 @@ VideoCutter::~VideoCutter() {
     delete ui;
 }
 
-QString* VideoCutter::getVideoFilePath() {
-    return &videoFilePath;
-}
 
-void VideoCutter::setSpinBoxValue(int value) {
-    ui->spinBox->setValue(value);
+void VideoCutter::setNumberInListValue(int number)
+{
+    numberInList = number;
 }
 
 void VideoCutter::setCheckBoxValue(bool value) {
     ui->checkBox->setChecked(value);
 }
 
-int VideoCutter::getSpinBoxValue() {
-    return(ui->spinBox->value());
+int VideoCutter::getNumberInListValue()
+{
+    return numberInList;
 }
 
 bool VideoCutter::getCheckBoxValue() {
     return(ui->checkBox->isChecked());
 }
 
+void VideoCutter::setNumberLabel(QString number)
+{
+    QFont font = ui->numberLabel->font();
+    font.setPointSize(12);
+    font.setBold(true);
+    ui->numberLabel->setFont(font);
+    ui->numberLabel->setText(number);
+}
+
+QString* VideoCutter::getVideoFilePath() {
+
+    return &videoFilePath;
+}
+
+void VideoCutter::setNameOfFile(QString filename) {
+
+    ui->nameOfFileLabel->setText(filename);
+}
+
+
+
 void VideoCutter::on_checkBox_stateChanged(int arg1) {
     if(getCheckBoxValue()) {
-    emit checkBoxStateChanged(getSpinBoxValue());
-    emit uploadVideo(getVideoFilePath(), false);
+        emit checkBoxStateChanged(getNumberInListValue());
+        emit uploadVideo(getVideoFilePath(), false);
     }
+}
+
+void VideoCutter::on_upBtn_clicked()
+{
+    emit upBtn(this,"MoveUp");
+}
+
+void VideoCutter::on_downBtn_clicked()
+{
+    emit downBtn(this,"MoveDown");
 }
--- gui/src/VideoCutterList.cpp
+++ gui/src/VideoCutterList.cpp
@@ -20,6 +20,9 @@ VideoCutterList::~VideoCutterList() {
 void VideoCutterList::getVideoFilePath() {
     videoFilePath = QFileDialog::getOpenFileName(this, tr("Open File"), "/",
                                                  tr("Video (*.avi *.mpeg *.mp4 *mkv)"));
+    QFile mfile(videoFilePath);
+    QString path = mfile.fileName();
+    fileName = path.section("/",-1,-1);
     emit uploadVideo(&videoFilePath, true);
 }
 
@@ -75,7 +78,7 @@ void VideoCutterList::videoLen(int length) {
 void VideoCutterList::onCheckBoxStateChanged(int number) {
     for(auto i : listOfVideoCutterWidgets) {
         VideoCutter* p = dynamic_cast<VideoCutter*>(i);
-        if(number != p->getSpinBoxValue()) {
+        if(number != p->getNumberInListValue()) {
             p->setCheckBoxValue(false);
         }
     }
@@ -85,8 +88,10 @@ void VideoCutterList::addNewCutter(QString *lengthOfFilm) {
     if(lengthOfFilm) {
         countOfVideo++;
         VideoCutter* m_VideoCutter = new VideoCutter(nullptr, countOfVideo, *lengthOfFilm, videoFilePath);
-
+        m_VideoCutter->setNumberLabel(QString::number(countOfVideo));
+        m_VideoCutter->setNumberInListValue(countOfVideo);
         m_VideoCutter->setCheckBoxValue(true);
+        m_VideoCutter->setNameOfFile(fileName);
         /* Делаем чекбоксы не активными */
         for (int i = 0; i < listOfVideoCutterWidgets.size(); i++) {
             listOfVideoCutterWidgets[i]->setCheckBoxValue(false);
@@ -94,10 +99,44 @@ void VideoCutterList::addNewCutter(QString *lengthOfFilm) {
 
         connect(m_VideoCutter, SIGNAL(checkBoxStateChanged(int)), this, SLOT(onCheckBoxStateChanged(int)));
         connect(m_VideoCutter, SIGNAL(uploadVideo(QString*, bool)), this, SIGNAL(uploadVideo(QString*, bool)));
+        connect(m_VideoCutter, SIGNAL(upBtn(QWidget*,QString)), this, SLOT(move(QWidget*,QString)));
+        connect(m_VideoCutter, SIGNAL(downBtn(QWidget*,QString)), this, SLOT(move(QWidget*,QString)));
 
         layout->addWidget(m_VideoCutter);
-
         this->setLayout(layout);
         listOfVideoCutterWidgets.append(m_VideoCutter);
     }
+}
+
+bool VideoCutterList::move(QWidget *widget, QString direction)
+{
+    QVBoxLayout* myLayout = qobject_cast<QVBoxLayout*>(widget->parentWidget()->layout());
+    int index = myLayout->indexOf(widget);
+    if (direction == "MoveUp" && index == 0) {
+        return false;
+    }
+
+    if (direction == "MoveDown" && index == myLayout->count()-1 ) {
+        return false;
+    }
+
+    int newIndex;
+    if(direction == "MoveUp") {
+         newIndex = index - 1;
+    } else {
+        newIndex = index + 1;
+    }
+
+    myLayout->removeWidget(widget);
+    myLayout->insertWidget(newIndex , widget);
+
+    for(auto i : listOfVideoCutterWidgets) {
+        VideoCutter* p = dynamic_cast<VideoCutter*>(i);
+        if(p->getNumberInListValue() != myLayout->indexOf(p) + 1) {
+            p->setNumberInListValue(myLayout->indexOf(p) + 1);
+            p->setNumberLabel(QString::number(myLayout->indexOf(p) + 1));
+        }
+    }
+
+    return true;
 }
